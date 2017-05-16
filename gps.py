e92268ebcf74ce0dea130e884510b7e4cc146436
Ryan Ledford <ryan.c.ledford@gmail.com>
2017-05-15 23:52:36 -0500

    updated doc strings and var names

--- gps.py
+++ gps.py
@@ -1,31 +1,30 @@
+"""provides functions for working with GPS strings"""
 def extract_lat_lng(var):
+    """extracts the latitude and longitude from NMEA GPS strings and converts to decimal degrees"""
     fields = var.split(',')
-    count = len(fields)
     lat = None
     lng = None
-
     for i in range(len(fields)):
         if i-1 >= 0:
             try:
-                f = fields[i]
-                if f == "N" or f == "S":
+                field = fields[i]
+                if field == "N" or field == "S":
                     degrees = float(fields[i-1][:2])
-                    decMin = float(fields[i-1][2:])
-                    lat = degrees + decMin/60.0
-                    if f == "S":
+                    dec_min = float(fields[i-1][2:])
+                    lat = degrees + dec_min/60.0
+                    if field == "S":
                         lat = lat * -1
-                if f == "E" or f == "W":
+                if field == "E" or field == "W":
                     degrees = float(fields[i-1][:3])
-                    decMin = float(fields[i-1][3:])
-                    lng = degrees + decMin/60.0
-                    if f == "W":
+                    dec_min = float(fields[i-1][3:])
+                    lng = degrees + dec_min/60.0
+                    if field == "W":
                         lng = lng * -1
             except ValueError:
-                print("invalid lat/lng data")
+                print("\ninvalid lat/lng data")
 
     if not lat or not lng:
-        print("ERROR: unable to parse GPS string")
+        print("\nERROR: unable to parse GPS string")
         return (None, None)
     else:
-        print("lat: %f\tlng: %f"%(lat, lng))
         return (lat, lng)
